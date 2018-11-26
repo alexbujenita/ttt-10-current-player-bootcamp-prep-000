c6457bd80aa0165a3259ff346b4fc06a3756dbd9
dekc89 <dekc89@gmail.com>
2018-11-26 19:43:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@ def turn_count board
   counter = 0
   board.each do |entry|
     if entry.count("a-zA-Z") > 0
-      counter += 1
-      puts entry
+      counter += 1      
     end
   end
   return counter
 end
+
